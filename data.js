f7d793dbc4053a2c7eb304bfa111191fe18ef22b
Nick Hellemans <NickHellemans93@gmail.com>
2021-12-13 14:10:42 +0100

    grouping & flipping order

--- data.js
+++ data.js
@@ -21,35 +21,67 @@ function getData() {
 		attached: false,
 	};
 
-	const columns = [
-		{
-			label: "To do",
-			id: "todo",
-		},
-		{
-			label: "In progress",
-			id: "inprogress",
-		},
-		{
-			label: "Done",
-			id: "done",
-		},
-	];
+	const columns = {
+        stakeholder: [
+            {
+                label: "Patient",
+                id: "patient",
+            },
+            {
+                label: "Nurse",
+                id: "nurse",
+            },
+            {
+                label: "Physician",
+                id: "physician",
+            },
+        ],
+        status: [
+            {
+                label: "To do",
+                id: "todo",
+            },
+            {
+                label: "In progress",
+                id: "inprogress",
+            },
+            {
+                label: "Done",
+                id: "done",
+            },
+        ]
+    }
 
-	const stakholders = [
-		{
-			label: "Patient",
-			id: "patient",
-		},
-		{
-			label: "Nurse",
-			id: "nurse"
-		},
-		{
-			label: "Physician",
-			id: "physician"
-		},
-	];
+	const rows = {
+        stakeholders: [
+            {
+                label: "Patient",
+                id: "patient",
+            },
+            {
+                label: "Nurse",
+                id: "nurse"
+            },
+            {
+                label: "Physician",
+                id: "physician"
+            },
+        ],
+        status: [
+            {
+                label: "To do",
+                id: "todo",
+            },
+            {
+                label: "In progress",
+                id: "inprogress",
+            },
+            {
+                label: "Done",
+                id: "done",
+            },
+        ]
+    }
 
 	const start_date = new Date("01/07/2021");
 	const end_date = new Date("01/15/2021");
@@ -58,59 +90,59 @@ function getData() {
 		{
 			label: "Activity #1",
 			start_date: new Date("01/07/2021"),
-			column: "todo",
+			status: "todo",
 			stakeholder: "patient",
 		},
 		{
 			label: "Activity #2",
-			column: "todo",
+			status: "todo",
 			stakeholder: "patient",
 		},
 		{
 			label: "Activity #3",
 			start_date: new Date("01/09/2021"),
-			column: "todo",
+			status: "todo",
 			stakeholder: "patient",
 		},
 		{
 			label: "Activity #4",
 			start_date: new Date("12/21/2020"),
-			column: "inprogress",
+			status: "inprogress",
 			stakeholder: "patient"
 		},
 		{
 			label: "Activity #5",
 			start_date: new Date("01/07/2021"),
-			column: "inprogress",
+			status: "inprogress",
 			stakeholder: "nurse",
 		},
 		{
 			label: "Activity #6",
 			start_date: new Date("12/21/2020"),
-			column: "inprogress",
+			status: "inprogress",
 			stakeholder: "nurse",
 		},
 		{
 			label: "Activity #7",
 			start_date: new Date("01/07/2021"),
-			column: "done",
+			status: "done",
 			stakeholder: "nurse",
 		},
 		{
 			label: "Activity #8",
-			column: "done",
+			status: "done",
 			stakeholder: "physician",
 		},
 		{
 			label: "Activity #9",
 			start_date: new Date("12/9/2020"),
-			column: "done",
+			status: "done",
 			stakeholder: "physician",
 		},
 	];
 
 	return {
-		rows: stakholders,
+		rows,
 		columns,
 		cards: activities,
 		cardShape,
